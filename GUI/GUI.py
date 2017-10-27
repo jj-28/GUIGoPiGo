d85909182eee970e32db493c54c4ce6330dafba9
Jeff <jeffreykjohnsonvb@gmail.com>
2017-10-27 13:42:26 -0400

    added source files for GUI

---
+++ GUI/GUI.py
@@ -0,0 +1,13 @@
+import sys
+import tkinter
+from tkinter import *
+
+App = Tk()
+
+App.geometry("600x800")
+
+App.mainloop()
+
+
+
+
---
+++ GUI/move.js
@@ -0,0 +1,36 @@
+function move(event) {
+    var k = event.keyCode,
+        chrId = document.getElementById('robot'),
+        chr = {
+            updown: function() {
+                var y = parseInt(getComputedStyle(chrId).top);
+                if (k == 38) {
+                    --y;
+                } else if (k == 40) {
+                    ++y;
+                }
+                return y;
+            },
+            leftright: function() {
+                var x = parseInt(getComputedStyle(chrId).left);
+                if (k == 37) {
+                    --x;
+                } else if (k == 39) {
+                    ++x;
+                }
+                return x;
+            }
+        };
+    chrId.style.top = (chr.updown()) + "px";
+    chrId.style.left = (chr.leftright()) + "px";
+
+}
+
+document.addEventListener('keydown', move);
+
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("demo").innerHTML = coords;
+}
