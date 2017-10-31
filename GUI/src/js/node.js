3e4f3f75d8c7339287ca3966c08fd3ea841a5893
Jeff <jeffreykjohnsonvb@gmail.com>
2017-10-31 13:08:48 -0400

    added 3 waypoints

---
+++ GUI/src/js/node.js
@@ -0,0 +1,15 @@
+//first make a function that adds and removes waypoint names from the map
+var queue = [];
+function addWaypoint(id) {
+    document.getElementsByName("n");
+
+}
+
+
+
+
+
+//then send those values as a python array, to the pathfinder
+
+
+//
