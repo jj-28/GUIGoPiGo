92eeb5ea53079120c2a819e31a62a27fb9b27dd7
Nathan O'Leary <nathanoleary1@gmail.com>
2017-11-09 05:34:53 -0500

    approximately 40 files and 400 lines of html added

--- GUI/src/js/move.js
+++ GUI/src/js/move.js
@@ -35,16 +35,4 @@ document.addEventListener('keydown', move);
 //     document.getElementById("demo").innerHTML = coords;
 // }
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
